99e44eb1817b3b4092e060c44abef67690ecfcf6
Gabriela Moura <gabriela.moura@aluno.impacta.edu.br>
2026-03-27 09:01:20 -0300

    Inclusão da função de subtração

--- minha_calculadora.py
+++ minha_calculadora.py
@@ -1,10 +1,16 @@
 def soma (a,b):
     return a + b
 
+def sub (a,b):
+    return a - b
+
 def main ():  # MAIN é a porta de entrada de um programa
     x = int (input ("x: "))
     y = int (input ("y: "))
     s = soma (x,y)
+    subt = sub (x,y)
     print (f"Soma = {s}")
+    print (f"Subtração = {subt}")
+
 
 main ()
